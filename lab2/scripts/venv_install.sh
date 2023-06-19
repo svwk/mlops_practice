5684febcb54349fc7f85ae1bd7989f0598a35e3e
SavSV <sv.ework@gmail.com>
2023-06-19 13:20:38 +0300

    bash venv files

--- lab2/scripts/venv_install.sh
+++ lab2/scripts/venv_install.sh
@@ -3,18 +3,6 @@
 # Каталог для скриптов python
 install_dir=$1
 
-if [[ ! -d "$install_dir" && ! -L "$install_dir" ]] ; then
-    echo "The specified install directory does not exist."
-    echo " The program is over."
-    exit 4
-fi
-
-if [ !  -r "$install_dir" ] ; then
-  echo "The  specified install  directory cannot be accessed."
-  echo "The program is over."
-  exit 5
-fi
-
 # Если venv не установлен:
 if [ $(dpkg-query -W -f='${Status}' 'python3-venv' | grep -c 'ok installed') -eq 0 ];
 then
@@ -26,9 +14,8 @@ then
  apt-get install -y python3-pip
 fi
 
-venv_dir="$install_dir/env"
-
 if [[ ! -d "$venv_dir" && ! -L "$venv_dir" ]] ; then
+  mkdir -m ug+rw -p "$venv_dir"
   python3 -m venv "$venv_dir"
 fi
 
